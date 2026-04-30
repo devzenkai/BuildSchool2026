9fe50a5183d0ec1bfe3bd650d9b1020d31e09b63
devzenkai <steven00041@gmail.com>
2026-04-30 10:23:20 +0800

    sync

---
+++ CSharp/Chapter3_Lab/LinqSamples/LinqSample015/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+namespace LinqSample015
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var list =
+                new List<string> { "Taipei", "Taipei", "Los Angeles", "New York", "New York", "Taipei" };
+            var result = list.Distinct();
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
