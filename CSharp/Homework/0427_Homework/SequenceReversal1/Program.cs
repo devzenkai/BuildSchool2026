fdcbeb5130e522f538d728f1bf666fe4cc3164f9
devzenkai <steven00041@gmail.com>
2026-05-04 18:06:31 +0800

    sync

---
+++ CSharp/Homework/0427_Homework/SequenceReversal1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace SequenceReversal1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            var result = string.Join(",", input.Split(",").Reverse());
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ CSharp/Homework/0427_Homework/SequenceReversal2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace SequenceReversal2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var result = Enumerable.Range(1, n).Select(i => new string((n - i + 1).ToString()[0], i));
+
+            foreach(var line in result)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
