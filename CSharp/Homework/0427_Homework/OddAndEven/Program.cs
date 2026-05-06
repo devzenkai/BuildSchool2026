71f75cc13d7d990d1423873c3dfdbdb683d09842
devzenkai <steven00041@gmail.com>
2026-05-06 09:15:29 +0800

    synce

--- CSharp/Homework/0427_Homework/OddAndEven/Program.cs
+++ CSharp/Homework/0427_Homework/OddAndEven/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string input = Console.ReadLine();
+
+            int[] numbers = input
+                .Split(",")
+                .Select(s => int.Parse(s.Trim()))
+                .ToArray();
+
+            int[] odds = numbers
+                .Where(n => n % 2 != 0)
+                .OrderBy(n => n)
+                .ToArray();
+
+            int[] evens = numbers
+                .Where(n => n % 2 == 0)
+                .OrderBy(n => n)
+                .ToArray();
+
+            Console.WriteLine($"Odds: {string.Join(",",odds)}");
+            Console.WriteLine($"Evens: {string.Join(",", evens)}");
         }
     }
 }
