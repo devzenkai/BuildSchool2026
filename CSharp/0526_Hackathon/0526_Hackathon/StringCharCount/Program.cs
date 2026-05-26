c5e9231f7a76bb3eb39b9599f53f8e88ef3596ea
devzenkai <steven00041@gmail.com>
2026-05-26 12:22:36 +0800

    String character count

---
+++ CSharp/0526_Hackathon/0526_Hackathon/StringCharCount/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace StringCharCount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+
+            var words = input
+                .Select(w => w.ToLower())
+                .GroupBy(w => w)
+                .ToDictionary(g => g.Key, g => g.Count());
+
+            foreach (var text in words)
+            {
+                Console.WriteLine($"{text.Key}: {text.Value}");
+            }
+        }
+    }
+}
